2b85cd75c716c15d5c3bee7feece6de8384fed55
Gaffarel <49238255+Gaffarel@users.noreply.github.com>
2020-02-01 13:15:47 +0100

    Mise à jour programme P9.sh Version 1.0

--- P9.sh
+++ P9.sh
@@ -2,7 +2,7 @@
 
 #####################################################################
 ##                                                                 ##
-##     Script de sauvegarde et restauration wordpresss  V0.30a     ##
+##     Script de sauvegarde et restauration wordpresss  V1.0       ##
 ##                                                                 ##
 #####################################################################
 
@@ -57,33 +57,64 @@ cd $BACKUP
      delete db_$BACKUPDATE_OLD.sql
      put save_$BACKUPDATE.tar.bz2
      put db_$BACKUPDATE.sql
-     put docker-compose.yml
-     ls save_*.tar.bz2 save_liste.txt
      quit
 FTP_CONNEX
 }
 
 function rest_ftp
 {
-   ftp -i -n $SERVEUR_FTP $PORT_FTP <<FTP_CONNEX
+   ftp -i -n $SERVEUR_FTP $PORT_FTP <<FTP_CONNEX1
      quote USER $USER_FTP
      quote PASS $MDP_FTP
      bin
      cd sauvegarde
-     get save_$BACKUPDATE.tar.bz2
-     get db_$BACKUPDATE.sql
      get docker-compose.yml
      get .env
-     get P9_config.ini
-     ls save_*.tar.bz2 save_liste.txt
      quit
-FTP_CONNEX
+FTP_CONNEX1
+}
+
+function list_ftp
+{
+   ftp -i -n $SERVEUR_FTP $PORT_FTP <<FTP_CONNEX2
+	quote USER $USER_FTP
+	quote PASS $MDP_FTP
+	bin
+	cd sauvegarde
+	ls db_*.sql save_db.txt
+	ls save_*.tar.bz2 save_liste.txt
+	quit
+FTP_CONNEX2
+}
+
+function recup_ftp1
+{
+   ftp -i -n $SERVEUR_FTP $PORT_FTP <<FTP_CONNEX3
+	quote USER $USER_FTP
+	quote PASS $MDP_FTP
+	bin
+	cd sauvegarde
+	get $choix
+	quit
+FTP_CONNEX3
+}
+
+function recup_ftp2
+{
+   ftp -i -n $SERVEUR_FTP $PORT_FTP <<FTP_CONNEX4
+	quote USER $USER_FTP
+	quote PASS $MDP_FTP
+	bin
+	cd sauvegarde
+	get $choix_db
+	quit
+FTP_CONNEX4
 }
 
 ####################### Test argument null ##########################
 
 if [[ $# -eq 0 ]] ; then
-    echo 'Manque un argument save / rest / rest-db / docker '
+    echo 'Manque un argument save / rest / docker '
     echo 'Pour sauvegarder votre serveur Wordpress et la BDD MariaDB: P9.sh save '
     echo 'Pour installer DOCKER: P9.sh docker '
     echo 'Pour restaurer votre serveur Wordpress en entier: P9.sh rest '
@@ -114,7 +145,7 @@ if [ "$1" = "save" ] ; then
     $DOCKER exec $contenaire_mariadb /usr/bin/mysqldump -u $USER_BDD --password=$MDP_BDD MyCompany > $BACKUP/db_$BACKUPDATE.sql
     echo "  Sauvegarde des Volumes Docker et des paramètres du réseau ......";
     sleep 2
-    $TAR cvpjf $BACKUP/save_$BACKUPDATE.tar.bz2 /var/lib/docker/volumes/backup_wp/ /etc/network/interfaces /etc/resolv.conf /etc/hosts /etc/hostname /var/spool/cron/crontabs/ /var/log/ $BACKUP/log/ $BACKUP/docker-compose.yml $BACKUP/db_$BACKUPDATE.sql
+    $TAR cvpjf $BACKUP/save_$BACKUPDATE.tar.bz2 /var/lib/docker/volumes/backup_wp/ /etc/network/interfaces /etc/resolv.conf /etc/hosts /etc/hostname /var/spool/cron/crontabs/ /var/log/ $BACKUP/log/ $BACKUP/docker-compose.yml
     echo "   Transfert vers le serveur FTP ...";
     sleep 2
     save_ftp
@@ -168,32 +199,69 @@ docker-compose up -d
 sleep 2
 
 #####################################################################
-########################### Restauration ############################
+####################### Restauration Totale #########################
 #####################################################################
 
 elif [ "$1" = "rest" ] ; then
 
+list_ftp
+
+echo "Qual Sauvegarde voulez-vous restaurer ?"
+fic=$( $CAT 'save_liste.txt' | awk '{print $9}')
+select choix in $(echo "$fic") ;
+	do echo "Mon choix est $choix :";
+	break;
+done
+
+recup_ftp1
+
 ## Restauration des Volumes Wordpress et des paramètres du réseau ###
 
 echo "Restauration des Volumes Docker et des paramètres du réseau ......";
-$TAR xvpjf $BACKUP/save_$BACKUPDATE.tar.bz2 -C /
+$TAR xvpjf $BACKUP/$choix -C /
 sleep 2
 ################## Restauration de la BDD MariaDB ###################
 
+#choix_db=$(db_$choix | cut -c3-11 )
+#ip=$(sudo ifconfig enp0s3 | grep 'inet 192.168.' | awk '{print $2}' | cut -c9-11)
+
 CONTAINER
-echo "Restauration de la BDD MariaDB ..."
-$CAT $BACKUP/db_$BACKUPDATE.sql | docker exec -i $contenaire_mariadb /usr/bin/mysql -u $USER_BDD -p$MDP_BDD MyCompany
-sleep 5
+
+echo "Restauration de la BDD ok ..."
+$CAT $BACKUP/db_*.sql | docker exec -i $contenaire_mariadb /usr/bin/mysql -u $USER_BDD -p$MDP_BDD MyCompany
+sleep 2
+
+#rm -f $BACKUP/db_*.sql
+#rm -f $BACKUP/save_*.tar.bz2
 
 #reboot
+
+#####################################################################
+######################### Restauration BDD ##########################
+#####################################################################
+
 
 elif [ "$1" = "rest_db" ] ; then
 
-CONTAINER
-echo "Restauration de la BDD MariaDB ..."
-$CAT $BACKUP/db_$BACKUPDATE.sql | docker exec -i $contenaire_mariadb /usr/bin/mysql -u $USER_BDD -p$MDP_BDD MyCompany
-sleep 5
+list_ftp
 
-#reboot
+echo "Restauration de la BDD MariaDB ..."
+fic=$( $CAT 'save_db.txt' | awk '{print $9}')
+select choix_db in $(echo "$fic") ;
+	do echo "Mon choix est $choix_db :";
+	break;
+done
+
+recup_ftp2
+
+CONTAINER
+
+echo "Restauration de la BDD ok ..."
+$CAT $BACKUP/$choix_db | docker exec -i $contenaire_mariadb /usr/bin/mysql -u $USER_BDD -p$MDP_BDD MyCompany
+sleep 2
+
+#rm -f $BACKUP/db_*.sql
+
+#reboot - non utile !
 
 fi
